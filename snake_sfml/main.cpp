6d0c58f6159f2ad703154bd4a639f3491a343ced
pofox <66141963+pofox@users.noreply.github.com>
2024-11-05 15:22:13 +0200

    importing level

--- snake_sfml/main.cpp
+++ snake_sfml/main.cpp
@@ -1,38 +1,68 @@
 #include <SFML/Graphics.hpp>
 #include <cmath>
 #include <vector>
+#include <fstream>
+#include <string>
 #include <iostream>
 
 struct node {
     sf::Vector2f olddir, dir, pos, targetPos;
     sf::CircleShape shape;
-    node() {
+    node(unsigned int cellsize) {
         olddir = dir = pos = targetPos = { 0, 0 };
-        shape.setRadius(45.0f);
+        shape.setRadius(0.45 * cellsize);
         shape.setFillColor(sf::Color::Green);
-        shape.setOrigin(sf::Vector2f(-7.5f, -7.5f));
+        shape.setOrigin(sf::Vector2f(-0.075 * cellsize, -0.075 * cellsize));
     }
 };
 
+sf::Vector2f scaleToFit(const sf::Vector2f& in, const sf::Vector2f& clip)
+{
+    sf::Vector2f ret(in);
+    if ((clip.y * in.x) / in.y >= clip.x)
+    {
+        ret.y = (clip.x * in.y) / in.x;
+        ret.x = clip.x;
+    }
+    else if ((clip.x * in.y) / in.x >= clip.y)
+    {
+        ret.x = (clip.y * in.x) / in.y;
+        ret.y = clip.y;
+    }
+    else
+        ret = clip;
+    return ret;
+}
+
+sf::Vector2f findPlayer();
+void fileToVec(std::string file);
+
+std::vector<std::vector<char> > level;
+
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(1080, 720), "Snake!", sf::Style::Close | sf::Style::Resize);
-    window.setFramerateLimit(60);
-    sf::RectangleShape rectangle[88];
+    fileToVec("assets/levels/1.txt");
+    unsigned int levelw = 0;
+    for (int i = 0; i < level.size(); i++) if (levelw < level[i].size()) levelw = level[i].size();
+    unsigned int levelh = level.size();
+    
+    unsigned int screenw = 1080;
+    unsigned int screenh = 720;
 
+    unsigned int cellsize = fmin(screenw / levelw, screenh / levelh);
+    
+    sf::RenderWindow window(sf::VideoMode(screenw, screenh), "Snake!", sf::Style::Close | sf::Style::Resize);
+    window.setFramerateLimit(60);
+    
+    
     std::vector<node> snake;
     float speed = 200.0f; // Snake movement speed per second
-    node head;
+    node head(cellsize);
+    head.pos = findPlayer() * (float)cellsize;
     head.targetPos = head.pos;
     snake.push_back(head);
 
     sf::Clock clock;
-    for (int i = 0; i < 88; i++) {
-        rectangle[i].setOrigin(sf::Vector2f(-5.0f, -5.0f));
-        rectangle[i].setSize(sf::Vector2f(95.0f, 95.0f));
-        rectangle[i].setFillColor(sf::Color(18, 18, 18, 255));
-        rectangle[i].setPosition(sf::Vector2f((i % 11) * 100.0f, ((int)(i / 11)) * 100.0f));
-    }
 
     sf::Vector2f newdir(0, 0);
 
@@ -51,12 +81,24 @@ int main()
             if (evnt.type == sf::Event::Closed) window.close();
             else if (evnt.type == sf::Event::TextEntered) {
                 if (evnt.text.unicode == 't' || evnt.text.unicode == 'T') {
-                    node body;
-                    body.pos = snake.back().pos - snake.back().dir * 100.0f;
+                    node body(cellsize);
+                    body.pos = snake.back().pos - snake.back().dir * (float)cellsize;
                     body.targetPos = snake.back().pos;
                     body.dir = snake.back().dir;
                     snake.push_back(body);
                 }
+            }else if (evnt.type == sf::Event::Resized)
+            {
+                // update the view to the new size of the window
+                unsigned int windoww = evnt.size.width;
+                unsigned int windowh = evnt.size.height;
+                int left = 0;
+                int top = 0;
+                sf::Vector2f r = scaleToFit(sf::Vector2f(screenw, screenh), sf::Vector2f(windoww, windowh));
+                left = (1 - windoww / r.x) * screenw / 2;
+                top = (1 - windowh / r.y) * screenh / 2;
+                sf::FloatRect visibleArea(left, top, (screenw * windoww) / r.x, (screenh * windowh) / r.y);
+                window.setView(sf::View(visibleArea));
             }
         }
 
@@ -70,7 +112,7 @@ int main()
             }
             snake[0].olddir = snake[0].dir;
             snake[0].dir = newdir;
-            snake[0].targetPos += snake[0].dir * 100.0f;
+            snake[0].targetPos += snake[0].dir * (float)cellsize;
         }
 
         // Move each segment towards its target position smoothly
@@ -83,21 +125,24 @@ int main()
             if (std::abs(n.targetPos.y - n.pos.y) < std::abs(moveStep.y)) moveStep.y = n.targetPos.y - n.pos.y;
 
             n.pos += moveStep;
-
-            // Update target for each segment to follow the previous segment
-            /*if (i > 0 && snake[i].pos == snake[i].targetPos) {
-                snake[i].dir = snake[i - 1].olddir;
-                snake[i].targetPos = snake[i - 1].pos;
-            }*/
         }
 
-        // Save current directions for all segments
-        /*for (auto& n : snake) {
-            n.olddir = n.dir;
-        }*/
-
         window.clear();
-        for (int i = 0; i < 88; i++) window.draw(rectangle[i]);
+        for (int i = 0; i < level.size(); i++)
+        {
+            for (int j = 0; j < (level[i]).size(); j++) {
+                sf::RectangleShape rectangle;
+                rectangle.setOrigin(sf::Vector2f(-0.05 * cellsize, -0.05 * cellsize));
+                rectangle.setSize(sf::Vector2f(0.95 * cellsize, 0.95 * cellsize));
+                rectangle.setFillColor(sf::Color(18, 18, 18, 255));
+                if (level[i][j] == '#')
+                {
+                    rectangle.setFillColor(sf::Color(100, 18, 180, 255));
+                }
+                rectangle.setPosition(sf::Vector2f(j * cellsize, i * cellsize));
+                window.draw(rectangle);
+            }
+        }
 
         // Draw all nodes (snake segments)
         for (auto& n : snake) {
@@ -109,4 +154,38 @@ int main()
     }
 
     return 0;
+}
+
+// player is '@'
+sf::Vector2f findPlayer()
+{
+    int i, j;
+    for (i = 0; i < level.size(); i++) {
+        for (j = 0; j < level[i].size(); j++) {
+            if (level[i][j] == '@')
+            {
+                return sf::Vector2f(j, i);
+            }
+        }
+    }
+
+}
+
+void fileToVec(std::string file)
+{
+    std::string line;
+    std::ifstream infile(file);
+
+    while (std::getline(infile, line)) {
+        std::vector<char> row;
+
+        for (char& c : line) {
+            if (c != ',') {
+                row.push_back(c);
+            }
+        }
+
+        level.push_back(row);
+    }
+
 }
