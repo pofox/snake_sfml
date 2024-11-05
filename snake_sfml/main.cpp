076f710c2413f7f1fb9ca50c92b9a1486b49496a
pofox <66141963+pofox@users.noreply.github.com>
2024-11-05 07:40:27 +0200

    snake is working

--- snake_sfml/main.cpp
+++ snake_sfml/main.cpp
@@ -1,69 +1,110 @@
 #include <SFML/Graphics.hpp>
+#include <cmath>
+#include <vector>
+#include <iostream>
+
+struct node {
+    sf::Vector2f olddir, dir, pos, targetPos;
+    sf::CircleShape shape;
+    node() {
+        olddir = dir = pos = targetPos = { 0, 0 };
+        shape.setRadius(45.0f);
+        shape.setFillColor(sf::Color::Green);
+        shape.setOrigin(sf::Vector2f(-7.5f, -7.5f));
+    }
+};
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(1080, 720), "SFML works!",sf::Style::Close | sf::Style::Resize);
+    sf::RenderWindow window(sf::VideoMode(1080, 720), "Snake!", sf::Style::Close | sf::Style::Resize);
+    window.setFramerateLimit(60);
     sf::RectangleShape rectangle[88];
 
-    for (int i = 0; i < 88; i++)
-    {
+    std::vector<node> snake;
+    float speed = 200.0f; // Snake movement speed per second
+    node head;
+    head.targetPos = head.pos;
+    snake.push_back(head);
+
+    sf::Clock clock;
+    for (int i = 0; i < 88; i++) {
         rectangle[i].setOrigin(sf::Vector2f(-5.0f, -5.0f));
         rectangle[i].setSize(sf::Vector2f(95.0f, 95.0f));
         rectangle[i].setFillColor(sf::Color(18, 18, 18, 255));
-        rectangle[i].setPosition(sf::Vector2f((i%11)*100.0f, ((int)(i/11))*100.0f));
+        rectangle[i].setPosition(sf::Vector2f((i % 11) * 100.0f, ((int)(i / 11)) * 100.0f));
     }
-    
-    sf::CircleShape shape(45.0f);
-    shape.setFillColor(sf::Color::Green);
-    shape.setOrigin(sf::Vector2f(-7.5f, -7.5f));
-    sf::Vector2f pos(0, 0);
+
+    sf::Vector2f newdir(0, 0);
 
     while (window.isOpen())
     {
+        sf::Time elapsed = clock.restart();
         sf::Event evnt;
-        while (window.pollEvent(evnt))
-        {
-            switch (evnt.type)
-            {
-            case sf::Event::Closed:
-                window.close();
-                break;
-            case sf::Event::TextEntered:
-                switch (evnt.text.unicode)
-                {
-                case 'W':
-                case 'w':
-                    pos.y -= 100;
-                    break;
-                case 'S':
-                case 's':
-                    pos.y += 100;
-                    break;
-                case 'A':
-                case 'a':
-                    pos.x -= 100;
-                    break;
-                case 'D':
-                case 'd':
-                    pos.x += 100;
-                    break;
-                default:
-                    break;
-                }
-                shape.setPosition(pos);
-                if (evnt.text.unicode < 128)
-                {
-                    printf("%c", evnt.text.unicode);
+
+        // Check for direction change with arrow keys
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up) && snake[0].dir.y != 1) newdir = sf::Vector2f(0, -1);
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down) && snake[0].dir.y != -1) newdir = sf::Vector2f(0, 1);
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right) && snake[0].dir.x != -1) newdir = sf::Vector2f(1, 0);
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left) && snake[0].dir.x != 1) newdir = sf::Vector2f(-1, 0);
+
+        while (window.pollEvent(evnt)) {
+            if (evnt.type == sf::Event::Closed) window.close();
+            else if (evnt.type == sf::Event::TextEntered) {
+                if (evnt.text.unicode == 't' || evnt.text.unicode == 'T') {
+                    node body;
+                    body.pos = snake.back().pos - snake.back().dir * 100.0f;
+                    body.targetPos = snake.back().pos;
+                    body.dir = snake.back().dir;
+                    snake.push_back(body);
                 }
             }
         }
 
-        window.clear();
-        for (int i = 0; i < 88; i++)
-        {
-            window.draw(rectangle[i]);
+        // Set target position for head if it reached the previous target
+        if (snake[0].pos == snake[0].targetPos) {
+            for (int i = snake.size() - 1; i > 0; i--)
+            {
+                snake[i].olddir = snake[i].dir;
+                snake[i].dir = snake[i - 1].dir;
+                snake[i].targetPos = snake[i - 1].pos;
+            }
+            snake[0].olddir = snake[0].dir;
+            snake[0].dir = newdir;
+            snake[0].targetPos += snake[0].dir * 100.0f;
         }
-        window.draw(shape);
+
+        // Move each segment towards its target position smoothly
+        for (size_t i = 0; i < snake.size(); ++i) {
+            node& n = snake[i];
+
+            // Move towards target position
+            sf::Vector2f moveStep = speed * n.dir * elapsed.asSeconds();
+            if (std::abs(n.targetPos.x - n.pos.x) < std::abs(moveStep.x)) moveStep.x = n.targetPos.x - n.pos.x;
+            if (std::abs(n.targetPos.y - n.pos.y) < std::abs(moveStep.y)) moveStep.y = n.targetPos.y - n.pos.y;
+
+            n.pos += moveStep;
+
+            // Update target for each segment to follow the previous segment
+            /*if (i > 0 && snake[i].pos == snake[i].targetPos) {
+                snake[i].dir = snake[i - 1].olddir;
+                snake[i].targetPos = snake[i - 1].pos;
+            }*/
+        }
+
+        // Save current directions for all segments
+        /*for (auto& n : snake) {
+            n.olddir = n.dir;
+        }*/
+
+        window.clear();
+        for (int i = 0; i < 88; i++) window.draw(rectangle[i]);
+
+        // Draw all nodes (snake segments)
+        for (auto& n : snake) {
+            n.shape.setPosition(n.pos);
+            window.draw(n.shape);
+        }
+
         window.display();
     }
 
